5f2c5d7dd0697ad93352b269d79beb099190da3f
ezio84 <brabus84@gmail.com>
2019-10-13 16:41:57 +0200

    Set abc paths

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -28,7 +28,7 @@ function breakfast()
     DU_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
+    for f in `/bin/ls vendor/nexus/vendorsetup.sh 2> /dev/null`
         do
             echo "including $f"
             . $f
