69c998f213a83085e6348f30713a5f421bb05b17
Surge1223 <Surge1223@gmail.com>
2020-09-21 20:14:43 +0000

    Allow mka and "make" to work again

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -17,7 +17,7 @@ function mka() {
             make -j `sysctl hw.ncpu|cut -d" " -f2` "$@"
             ;;
         *)
-            schedtool -B -n 1 -e ionice -n 1 make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
+            make -j `cat /proc/cpuinfo | grep "^processor" | wc -l` "$@"
             ;;
     esac
 }
@@ -28,8 +28,8 @@ function breakfast()
     DU_DEVICES_ONLY="true"
     unset LUNCH_MENU_CHOICES
     add_lunch_combo full-eng
-    for f in `/bin/ls vendor/du/vendorsetup.sh 2> /dev/null`
-        do
+       for f in $(test -d $ANDROID_BUILD_TOP/vendor/du && \
+            find -L $ANDROID_BUILD_TOP/vendor/du  -maxdepth 4 -name 'vendorsetup.sh' 2>/dev/null | sort); do
             echo "including $f"
             . $f
         done
